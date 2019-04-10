507869182887c9e49203b893e95ba04f98db43f5
RealTradeDev <realtradexcoin@gmail.com>
2019-04-10 14:53:30 +0800

    initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4a, 0x21, 0x29, 0x5d, 0xd4, 0x33 0x1a, 0x11, 0x34, 0x41, 0x0a, 0x1d, 0x98, 0xdd, 0xa3, 0xa4 } };
+}
